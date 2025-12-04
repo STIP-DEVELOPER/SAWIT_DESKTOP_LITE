769c80d624e723c9a26eaabe13b0770bfb3d406b
misdarmanto <misdarmanto1922@gmail.com>
2025-12-05 00:30:55 +0700

    port

--- src/utils/serial.py
+++ src/utils/serial.py
@@ -4,4 +4,5 @@ import serial.tools.list_ports
 
 def get_serial_ports():
     ports = serial.tools.list_ports.comports()
+    print("found serial ports:", [p.device for p in ports])
     return [p.device for p in ports]
