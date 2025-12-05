23bb2a04414ecdc8d46cd648e257c1b5307a9f93
misdarmanto <misdarmanto1922@gmail.com>
2025-12-06 02:43:29 +0700

    s

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -5,7 +5,9 @@ from threading import Lock
 
 from utils.path import ROOT_DIR
 
-LOG_FILE = os.path.join(ROOT_DIR, "logs", "data.json")
+LOG_FILE = os.path.join(ROOT_DIR, "/logs", "data.json")
+
+print(f"[Logger] Log file path: {LOG_FILE}")
 
 
 _log_lock = Lock()
