529ca27f120c3d4b7cad050efce05f45a7fb2b1b
misdarmanto <misdarmanto1922@gmail.com>
2025-12-06 12:21:30 +0700

    feat: adjust directory path

--- src/utils/path.py
+++ src/utils/path.py
@@ -2,7 +2,7 @@ import os
 from os.path import dirname, join
 
 ROOT = dirname(dirname(__file__))
-ICON_DIR = join(ROOT, "assets", "icons")
+ICON_DIR = join(ROOT, "src", "assets", "icons")
 
 
 ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
