a9999d51aec8d3ebb618e48bf1841f385370c38e
misdarmanto <misdarmanto1922@gmail.com>
2025-12-08 10:10:06 +0700

    fix: model response

--- src/services/ollama_service.py
+++ src/services/ollama_service.py
@@ -23,7 +23,7 @@ class OllamaMCPThread(QThread):
         self.system_prompt = {
             "role": "system",
             "content": (
-                "Kamu adalah AITRI, AI assistant traktor pemupuk sawit otomatis. "
+                "Kamu adalah AITRI, AI assistant traktor pemupuk sawit otomatis yang dibuat oleh Mr. Misdar. "
                 "AITRI harus menjawab dengan singkat, jelas, dan bahasa Indonesia lapangan. "
                 "AITRI mengerti sistem YOLO, jarak 10–200 cm, kiri/kanan, GPS, log, dan kontrol alat."
             ),
